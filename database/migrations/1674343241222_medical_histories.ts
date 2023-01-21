2108cc97230fdda508ec3c7dfe0b54f26ae75f83
cesar zambrano <cesarzambranod@gmail.com>
2023-01-21 20:23:41 -0300

    feat: mediacl history

---
+++ database/migrations/1674343241222_medical_histories.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'medical_histories'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.string('description')
+      table.integer('pets_id').unsigned().references('id').inTable('pets').onDelete('CASCADE')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
